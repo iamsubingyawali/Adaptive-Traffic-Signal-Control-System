2ee17378214d1b6be984ed37beae1e9ad1163927
iamsubingyawali <subingyawali7@gmail.com>
2021-04-20 16:50:59 +0545

    Minor changes

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-call python multithreading.py "/videos/5.mp4" >> out.txt
-call python multithreading.py "/videos/6.mp4" >> out.txt
-call python multithreading.py "/videos/7.mp4" >> out.txt
-call python multithreading.py "/videos/8.mp4" >> out.txt
+call python multithreading.py "/videos/1.mp4" >> out.txt
+call python multithreading.py "/videos/2.mp4" >> out.txt
+call python multithreading.py "/videos/3.mp4" >> out.txt
+call python multithreading.py "/videos/4.mp4" >> out.txt
 call python program.py
