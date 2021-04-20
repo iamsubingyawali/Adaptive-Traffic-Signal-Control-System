207d1a42eb989a343b744cf537a5533f83a03491
iamsubingyawali <subingyawali7@gmail.com>
2021-04-20 16:31:18 +0545

    Added batch script to run program on windows

---
+++ run.bat
@@ -0,0 +1,5 @@
+call python multithreading.py "/videos/5.mp4" >> out.txt
+call python multithreading.py "/videos/6.mp4" >> out.txt
+call python multithreading.py "/videos/7.mp4" >> out.txt
+call python multithreading.py "/videos/8.mp4" >> out.txt
+call python program.py
